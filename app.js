409cfc18a1413a8f6f323fdc7c423af4794240ea
vladi227 <vladimorales62@gmail.com>
2021-08-08 23:50:32 -0300

    cambios en app.js y notas de inicioSL

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const path = require('path');
 
-app.get('/',(req,res)=> res.send('por fin estamos en el navegador XD'))
-app.get('/contacto',(req,res)=>res.send('CONTACTO'))
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/contacto',(req,res)=>res.send('CONTACTO'));
 
 app.listen(3030, ()=> console.log('Servidor corriendo en el puerto ' + port));
